0fa3e16f8cbae381ec9494204f29a7662293a28b
Nikola Lukic <nikola@lukic.biz>
2018-12-04 20:34:41 -0500

    change to 719 scrape

--- patch_719_scrape.py
+++ patch_719_scrape.py
@@ -13,8 +13,6 @@ for id in match_ids:
     r = requests.get(f"https://api.opendota.com/api/matches/{id}")
     r = r.json()
 
-    matches.append(r)
-    time.sleep(1.5)
-
-with open('data/patch_719_data.json', 'w') as outfile:
-    json.dump(matches, outfile)
+    with open(f'data/patch_719_scrape/{id}.json', 'w') as outfile:
+        json.dump(r, outfile)
+    time.sleep(1.1)
