724f57d0af6d4a2e66c17b9ef9b17e8902e13328
Nikola Lukic <nikola@lukic.biz>
2018-12-04 21:12:11 -0500

    added generalized scraper

---
+++ request_matches.py
@@ -0,0 +1,30 @@
+import requests
+import json
+import pandas as pd
+import time
+import logging
+
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
+
+
+def get_matches(match_ids_csv):
+    """takes the name of a csv containing Dota 2 match ids (under a column
+    "match_id") and returns the matches as json objects.
+
+    """
+    df = pd.read_csv(f'{match_ids_csv}.csv')
+    match_ids = df['match_id'].values.tolist()
+
+    s = requests.Session()
+    retries = Retry(total=None, backoff_factor=1, status_forcelist=[ 502, 503, 504 ])
+    s.mount('http://', HTTPAdapter(max_retries=retries))
+
+    for id in match_ids:
+        r = s.get(f"https://api.opendota.com/api/matches/{id}")
+        r = r.json()
+
+        with open(f'data/patch_719_scrape/{id}.json', 'w') as outfile:
+            json.dump(r, outfile)
+        time.sleep(1.1)
